5ab770b55b8dd3917199c0bbb575cb06ae0e83d5
tux2208 <111603396+tux2208@users.noreply.github.com>
2023-08-13 14:07:41 -0600

    Update lspconfig.lua

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -4,3 +4,16 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require("lspconfig")
 local util = require "lspconfig/util"
 
+lspconfig.rust_analyzer.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = {"rust"},
+  root_dir = util.root_pattern("Cargo.toml"),
+  settings = {
+    ['rust_analyzer'] = {
+      cargo = {
+        allFeatures = true,
+      },
+    },
+  },
+})
